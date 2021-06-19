22d5ef34c8f1c21ff6a52ab30a64130f4a324ad0
sun5415 <kwonsunseung@gmail.com>
2021-06-19 16:36:09 +0900

    6주차 과제 제출

---
+++ venv/6주차 과제 제출.py
@@ -0,0 +1,15 @@
+import bs4
+import requests
+
+headers = {'User-Agent': 'Not_Crawling X)'}
+
+response = requests.get('https://kin.naver.com/',headers=headers).text
+soup=bs4.BeautifulSoup(response, 'html.parser')
+
+ranks = soup.select('#rankingChart > ul > li')
+
+with open('naver_kin_rank.csv','w') as f:
+    for rank in ranks:
+        num = rank.select_one('span.no').text
+        title = rank.select_one('a.ranking_title').text
+        f.write(f'{num},{title}\n')
